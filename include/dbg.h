027c1dafe0d99ee0f674f3825c671a6b66de9702
Louie Caulfield <louie.caulfield@gmail.com>
2014-01-18 16:40:22 +0100

    include/dbg.h: made separate macros for reporting errors from errno

--- include/dbg.h
+++ include/dbg.h
@@ -13,18 +13,22 @@
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M " \n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_err_errno(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M " \n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d errno: %s) " M " \n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_warn_errno(M, ...) fprintf(stderr, "[WARN] (%s:%d errno: %s) " M " \n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
-
-#define check(A, M, ...) if(!(A)) {log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+#define check_errno(A, M, ...) if(!(A)) {log_err_errno(M, ##__VA_ARGS__); errno=0; goto error; }
 
 #define sentinel(M, ...) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 
-#define check_mem(A) check((A), "Out of memory.");
+#define check_mem(A) check_errno((A), "Out of memory.");
 
-#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__); errno=0; goto error; }
+#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d) " M " \n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d) " M " \n", __FILE__, __LINE__,  ##__VA_ARGS__)
+
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+#define check(A, M, ...) if(!(A)) {log_err(M, ##__VA_ARGS__); goto error; }
 
 #endif

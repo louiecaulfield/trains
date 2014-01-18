e27a1e5298217a09a6ef67f5c7424df3b9c53eef
Louie Caulfield <louie.caulfield@gmail.com>
2014-01-18 12:06:15 +0100

    main.c: get dbfile and stations from command line args

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <curl/curl.h>
 #include <tidy/tidy.h>
 #include <tidy/buffio.h>
+#include <unistd.h>
 #include "trains.h"
 #include "include/dbg.h"
 #include "html.h"
@@ -13,7 +14,9 @@
 
 int main(int argc, char *argv[])
 {
-	int res, consecutive_success = 0;
+	//CL args
+	const char *dbfile = NULL, *stn_departure = NULL, *stn_arrival = NULL;
+	int ch,res, consecutive_success = 0;
 
 	//Handles
 	CURL *curl_hdl = NULL;
@@ -23,28 +26,53 @@ int main(int argc, char *argv[])
 	//Query initialisers
 	char *link, *new_link;
 	struct tm time_dep;
-	char *stn_departure = NULL, *stn_arrival = NULL;
 
 	//Parse result holders
 	struct train_list_t *trains = NULL;
 	size_t n, ntrains;
 
 	//Parse cmdline
-	if(argc != 2) {
-		printf(" usage: %s <db>\n", argv[0]);
-		return -1;
+	while( (ch = getopt(argc, argv, "d:f:t:")) != -1 ) {
+		debug("ch = %d", ch);
+		switch(ch) {
+		case 'd':
+			debug("d %s", optarg);
+			dbfile = optarg;
+			break;
+		case 'f':
+			debug("f %s", optarg);
+			stn_departure = optarg;
+			break;
+		case 't':
+			debug("t %s", optarg);
+			stn_arrival = optarg;
+			break;
+		case '?':
+			if(optopt=='d' || optopt=='f' || optopt=='t') {
+				log_err("Missing argument for option -%c", optopt);
+				goto usage;
+			} else if(isprint(optopt)) {
+				log_info("Unknown option '-%c'", optopt);
+			} else {
+				log_info("Unknown option character '\\x%x'", optopt);
+			}
+			break;	
+		default:
+			debug("err got c=%d (opterr=%d, optopt=%c, optind=%d, optarg=%s)", ch, opterr, optopt, optind, optarg);
+			goto usage;	
+		}
 	}
+	if(!dbfile || !stn_departure || !stn_arrival) goto usage;
+	log_info("Starting %s with dbfile='%s', stn_dep='%s', stn_arr='%s'",
+		argv[0], dbfile, stn_departure, stn_arrival);
 
 	//Set up Curl 
 	check(curl_tidy_init(&curl_hdl)==0,"Failed to initialise curl");
 	debug("curl_hdl %p", curl_hdl);
 
 	//Set up database and get names
-	res = database_init(&db_hdl, argv[1]);
+	res = database_init(&db_hdl, dbfile);
 	check(res==0, "Failed to open database"); 
-
-	asprintf(&stn_departure, "Brussels Midi");
-	asprintf(&stn_arrival, "Paris Nord");
 
 	//Send search query 
 	time_dep.tm_sec = time_dep.tm_min = 0;
@@ -112,8 +140,6 @@ int main(int argc, char *argv[])
 	}
 	free(link);
 
-	free(stn_departure);
-	free(stn_arrival);
 	if(tdoc) {
 		tidySaveFile(tdoc, "dumpfile-exit.html");
 		tidyRelease(tdoc);
@@ -123,8 +149,6 @@ int main(int argc, char *argv[])
 	return 0;
 
 error:
-	free(stn_departure);
-	free(stn_arrival);
 	if(tdoc) {
 		tidySaveFile(tdoc, "dumpfile-exit.html");
 		tidyRelease(tdoc);
@@ -132,4 +156,14 @@ error:
 	curl_tidy_cleanup(curl_hdl);
 	database_cleanup(db_hdl);
 	return -1;
+usage:
+	printf(
+		"Usage : %s -d <dbfile> -f <stn_dep> -t <stn_arr>\n"
+		"\n"
+		"\t<dbfile>\tThe sqlite3 database filename\n"
+		"\t<stn_dep>\tThe departure station\n"
+		"\t<stn_arr>\tThe arrival station\n"
+		"\n",
+	argv[0]);
+	return 0;
 }

d161103c116692e39cd8893e2872d62046dadd4e
Louie Caulfield <louie.caulfield@gmail.com>
2014-01-21 15:06:13 +0100

    curl_tidy.h, database.h: documented functions

--- curl_tidy.h
+++ curl_tidy.h
@@ -4,15 +4,43 @@
 #include <tidy/tidy.h>
 #include <tidy/buffio.h>
 
-size_t write_cb(char *in, size_t size, size_t nmemb, TidyBuffer *out);
-
+//! Initialise curl
+/*! 
+	\param curl_hdl The created curl handle
+	\return 0 for success, -1 upon failure
+*/
 int curl_tidy_init(CURL **curl_hdl);
+//! Destroys the curl handle
+/*!
+	\param curl_hdl The curl handle that needs to be cleaned up
+*/
 void curl_tidy_cleanup(CURL *curl_hdl);
 
+//! Read an html file into a tidyHtml document, parse and clean the html
+/*!
+	\param filename A string containing with the html file name, used by fopen
+	\param tdoc The Tidy document where the cleaned up document will be stored 
+	\return 0 for successs, -1 upon failure
+*/
 int curl_tidy_read(char *filename, TidyDoc *tdoc);
 
+//! Sends a HTTP POST request with postfields and returns the html
+/*!
+	\param curl_hdl The curl handle to use
+	\param url A string containing the URL
+	\param postfields Url encoded post fields
+ 	\param tdoc The Tidy document where the cleaned up response will be stored
+	\return 0 for success, -1 upon failure
+*/
 int curl_tidy_post(CURL *curl_hdl, const char * url, char* postfields, TidyDoc *tdoc);
 
+//! Sends a HTTP GET request and returns the html
+/*!
+	\param curl_hdl The curl handle to use
+	\param url A string containing the URL
+ 	\param tdoc The Tidy document where the cleaned up response will be stored
+	\return 0 for success, -1 upon failure
+*/
 int curl_tidy_get(CURL *curl_hdl, const char * url, TidyDoc *tdoc);
 
 #endif
--- database.h
+++ database.h
@@ -4,14 +4,62 @@
 #include <sqlite3.h>
 #include "trains.h"
 
+//! Open sqlite3 database and return handle
+/*
+	\param db_hdl Pointer to *db_hdl in which the created db handle will be stored
+	\param db_file String containing the database filename
+	\return 0 for success, -1 for failure
+*/
 int database_init(sqlite3 **db_hdl, const char *dbfile);
+//! Close sqlite3 database
+/*
+	\param db_hdl database handle to close
+*/
 void database_cleanup(sqlite3 *db_hdl);
 
+//! Find a station in the sqlite3 table by name
+/*
+	\param db_hdl The sqlite3 handle
+	\param query The station name you're looking for, will try to find stations LIKE 'query%'
+	\param name [out]The string of the found station name, can be NULL if you don't care
+	\param id [out]The id of the found station, can be NULL if you don't care
+	\result 0 if station found, -1 if no station found
+*/
 int station_find(sqlite3 *db_hdl, const char *query, char **name, int* id);
+
+//! Get a station from the sqlite3 table by id
+/*
+	\param db_hdl The sqlite3 handle
+	\param id The id of the row
+	\param name [out]The string of the found station name, can be NULL if you don't care
+	\result 0 if station found, -1 if no station found
+*/
 int station_get(sqlite3 *db_hdl, int id, char **name);
+
+//! Get a station from the sqlite3 table by id
+/*
+	\param db_hdl The sqlite3 handle
+	\param name The string of the new station name
+	\result 0 if station insterted, -1 if failed 
+*/
 int station_insert(sqlite3 *db_hdl, const char *name);
+
+//! Try to find a station by name and insert if not found
+/*
+	\param db_hdl The sqlite3 handle
+	\param query String containing the station name you're looking for
+	\param name [out]Station name found in the database, can be NULL
+	\param id [out]Station id found in database, can be NULL	
+	\return 0 if station inserted and then found, -1 if failure
+*/
 int station_find_or_insert(sqlite3 *db_hdl, const char *query, char **name, int* id);
 
+//! Store linked list of trains in database
+/*
+	\param db_hdl The sqlite3 handle
+	\param trains The head node of the linked list you're storing
+	\return number of nodes stored
+*/
 size_t train_store(sqlite3 *db_hdl, struct train_list_t *trains);
 
 #endif
